babeeefdc20230f4d0ee6aa877abd8f9e031db06
jinwoong16 <baseball0020@gmail.com>
2024-08-08 09:42:27 +0900

    Initial Commit

---
+++ Kabinett/KabinettApp.swift
@@ -0,0 +1,17 @@
+//
+//  KabinettApp.swift
+//  Kabinett
+//
+//  Created by jinwoong Kim on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct KabinettApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
