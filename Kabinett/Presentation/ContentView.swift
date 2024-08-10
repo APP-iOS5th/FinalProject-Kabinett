d37c35bc30fc3f6a93aa9a7428e871f423246f96
jinwoong <baseball0020@gmail.com>
2024-08-10 19:51:30 +0900

    Chore: Firebase 초기화 구문 추가
    - 파이어베이스 로컬 에뮬레이터를 사용할 때, 주석처리된 부분을 해제해주세요

--- Kabinett/Presentation/ContentView.swift
+++ Kabinett/Presentation/ContentView.swift
@@ -6,8 +6,23 @@
 //
 
 import SwiftUI
+import FirebaseCore
+// import FirebaseAuth
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+//        Auth.auth().useEmulator(withHost: "localhost", port: 9099)
+        
+        return true
+    }
+}
+
 
 struct ContentView: View {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
