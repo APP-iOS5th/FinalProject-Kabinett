5b3d7ce558f6331f8d2ec1da6d5413fb828b0a5b
YuleGlycerine <figurefig13@gmail.com>
2024-08-13 21:06:40 +0900

    Feat: ProfileView 추가

---
+++ Kabinett/Presentation/View/Profile/ProfileView.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileView.swift
+//  Kabinett
+//
+//  Created by Yule on 8/13/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        VStack{
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.background)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
