20b4dd54126469ba528a93bc164d5c22bc9ba246
the-hye <ganzi410@gmail.com>
2024-08-13 18:10:54 +0900

    Refactor: FirebaseLetterService 중복 코드 통합

--- Kabinett/Service/Firebase/FirebaseLetterService.swift
+++ Kabinett/Service/Firebase/FirebaseLetterService.swift
@@ -16,14 +16,22 @@ enum LetterError: Error {
     case invalidToUserId
     case invalidPhtoContents
     case invalidStationery
-    case invalidFromUser
-    case invalidToUser
+}
+
+enum LetterSaveError: Error {
+    case invalidFromUserDoc
+    case invalidToUserDoc
+    case failedToSaveToMe
+    case failedToSaveSent
+    case failedToSaveReceived
+    case failedToSaveBoth
 }
 
 final class FirebaseLetterService: LetterWriteUseCase, ComponentsUseCase {
     
     let db = Firestore.firestore()
     
+    // LetterWriteUseCase
     func saveLetter(font: String, postScript: String?, envelope: String, stamp: String,
                     fromUserId: String, toUserId: String, content: String?, photoContents: [String], date: Date,
                     stationery: String, isRead: Bool) async -> Result<Void, any Error> {
@@ -36,130 +44,123 @@ final class FirebaseLetterService: LetterWriteUseCase, ComponentsUseCase {
         guard !toUserId.isEmpty else { return .failure(LetterError.invalidToUserId) }
         guard !stationery.isEmpty else { return .failure(LetterError.invalidStationery) }
         
-        // MARK: - Letter 객체 생성
-        let letter = Letter(
-            id: nil,
-            fontString: font,
-            postScript: postScript ?? "",
-            envelopeImageUrlString: envelope,
-            stampImageUrlString: stamp,
-            fromUserId: fromUserId,
-            toUserId: toUserId,
-            content: content ?? "",
-            photoContents: photoContents,
-            date: date,
-            stationeryImageUrlString: stationery,
-            isRead: isRead)
-        
-        // MARK: - Firestore 저장
-        let fromUserDoc = db.collection("Writer").document(fromUserId)
-        let toUserDoc = db.collection("Writer").document(toUserId)
-        
-        if fromUserId == toUserId {
-            do {
-                let snapshot = try await fromUserDoc.getDocument()
-                guard snapshot.exists else {
-                    return .failure(LetterError.invalidFromUser)
-                }
-                
-                let letterData = try Firestore.Encoder().encode(letter)
-                
-                try await fromUserDoc.collection("ToMe").addDocument(data: letterData)
-                return .success(())
-                
-            } catch {
-                return .failure(error)
-            }
-        } else {
-            do {
-                let snapshotFrom = try await fromUserDoc.getDocument()
-                let snapshotTo = try await toUserDoc.getDocument()
-                
-                guard snapshotFrom.exists else {
-                    return .failure(LetterError.invalidFromUser)
-                }
-                guard snapshotTo.exists else {
-                    return .failure(LetterError.invalidToUser)
-                }
-                
-                let letterData = try Firestore.Encoder().encode(letter)
-                
-                try await fromUserDoc.collection("Sent").addDocument(data: letterData)
-                try await toUserDoc.collection("Received").addDocument(data: letterData)
-                
-                return .success(())
-                
-            } catch {
-                return .failure(error)
-            }
+        do {
+            try await validateUsers(fromUserId: fromUserId, toUserId: toUserId)
+            
+            let letter = Letter(
+                id: nil,
+                fontString: font,
+                postScript: postScript ?? "",
+                envelopeImageUrlString: envelope,
+                stampImageUrlString: stamp,
+                fromUserId: fromUserId,
+                toUserId: toUserId,
+                content: content ?? "",
+                photoContents: photoContents,
+                date: date,
+                stationeryImageUrlString: stationery,
+                isRead: isRead)
+            
+            return await saveLetterToFireStore(letter: letter, fromUserId: fromUserId, toUserId: toUserId)
+            
+        } catch {
+            return .failure(error)
         }
     }
     
+    // ComponentsUsesCase
     func saveLetter(postScript: String?, envelope: String, stamp: String, fromUserId: String, toUserId: String, photoContents: [String], date: Date, isRead: Bool) async -> Result<Void, any Error> {
         
         // MARK: - Parameter 유효성 검사
         guard !envelope.isEmpty else { return .failure(LetterError.invalidEnvelope) }
         guard !stamp.isEmpty else { return .failure(LetterError.invalidStamp) }
         guard !fromUserId.isEmpty else { return .failure(LetterError.invalidFromUserId) }
-        guard !photoContents.isEmpty else { return .failure(LetterError.invalidPhtoContents) }
         guard !toUserId.isEmpty else { return .failure(LetterError.invalidToUserId) }
+        guard !photoContents.isEmpty else { return .failure(LetterError.invalidPhtoContents) }
         
-        // MARK: - Letter 객체 생성
-        let letter = Letter(
-            id: nil,
-            fontString: nil,
-            postScript: postScript ?? "",
-            envelopeImageUrlString: envelope,
-            stampImageUrlString: stamp,
-            fromUserId: fromUserId,
-            toUserId: toUserId,
-            content: nil,
-            photoContents: photoContents,
-            date: date,
-            stationeryImageUrlString: nil,
-            isRead: isRead)
-        
-        // MARK: - Firestore 저장
+        do {
+            try await validateUsers(fromUserId: fromUserId, toUserId: toUserId)
+            
+            let letter = Letter(
+                id: nil,
+                fontString: nil,
+                postScript: postScript ?? "",
+                envelopeImageUrlString: envelope,
+                stampImageUrlString: stamp,
+                fromUserId: fromUserId,
+                toUserId: toUserId,
+                content: nil,
+                photoContents: photoContents,
+                date: date,
+                stationeryImageUrlString: nil,
+                isRead: isRead)
+            
+            return await saveLetterToFireStore(letter: letter, fromUserId: fromUserId, toUserId: toUserId)
+            
+        } catch {
+            return .failure(error)
+        }
+    }
+    
+    // MARK: - 유저 유효성 검사
+    private func validateUsers(fromUserId: String, toUserId: String) async throws {
         let fromUserDoc = db.collection("Writer").document(fromUserId)
         let toUserDoc = db.collection("Writer").document(toUserId)
         
-        if fromUserId == toUserId {
-            do {
-                let snapshot = try await fromUserDoc.getDocument()
-                guard snapshot.exists else {
-                    return .failure(LetterError.invalidFromUser)
-                }
-                
-                let letterData = try Firestore.Encoder().encode(letter)
-                
-                try await fromUserDoc.collection("ToMe").addDocument(data: letterData)
-                return .success(())
-                
-            } catch {
-                return .failure(error)
-            }
-        } else {
-            do {
-                let snapshotFrom = try await fromUserDoc.getDocument()
-                let snapshotTo = try await toUserDoc.getDocument()
-                
-                guard snapshotFrom.exists else {
-                    return .failure(LetterError.invalidFromUser)
-                }
-                guard snapshotTo.exists else {
-                    return .failure(LetterError.invalidToUser)
-                }
-                
-                let letterData = try Firestore.Encoder().encode(letter)
-                
-                try await fromUserDoc.collection("Sent").addDocument(data: letterData)
-                try await toUserDoc.collection("Received").addDocument(data: letterData)
-                
-                return .success(())
-                
-            } catch {
-                return .failure(error)
-            }
+        let snapshotFrom = try await fromUserDoc.getDocument()
+        guard snapshotFrom.exists else { throw LetterSaveError.invalidFromUserDoc }
+        
+        if fromUserId != toUserId {
+            let snapshotTo = try await toUserDoc.getDocument()
+            guard snapshotTo.exists else { throw LetterSaveError.invalidToUserDoc }
         }
+    }
+    
+    // MARK: - Firestore Letter 저장
+    private func saveLetterToFireStore(letter: Letter, fromUserId: String, toUserId: String) async -> Result<Void, Error> {
+        
+        do {
+            let fromUserDoc = db.collection("Writer").document(fromUserId)
+            let toUserDoc = db.collection("Writer").document(toUserId)
+            
+            let letterData = try Firestore.Encoder().encode(letter)
+            
+            if fromUserId == toUserId {
+                do {
+                    try await fromUserDoc.collection("ToMe").addDocument(data: letterData)
+                    return .success(())
+                } catch {
+                    return .failure(LetterSaveError.failedToSaveToMe)
+                }
+            } else {
+                var sentSaveError: Error?
+                var receivedSaveError: Error?
+                
+                do {
+                    try await fromUserDoc.collection("Sent").addDocument(data: letterData)
+                } catch {
+                    sentSaveError = error
+                }
+                
+                do {
+                    try await fromUserDoc.collection("Recieved").addDocument(data: letterData)
+                } catch {
+                    receivedSaveError = error
+                }
+                
+                if let _ = sentSaveError, let _ = receivedSaveError {
+                    return .failure(LetterSaveError.failedToSaveBoth)
+                } else if let _ = sentSaveError {
+                    return .failure(LetterSaveError.failedToSaveSent)
+                } else if let _ = receivedSaveError {
+                    return .failure(LetterSaveError.failedToSaveReceived)
+                }
+                
+                return .success(())
+            }
+        } catch {
+            return .failure(error)
+        }
+        
     }
 }
