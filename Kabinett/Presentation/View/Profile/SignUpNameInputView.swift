b65d1217c1b3fc31c9ab71830c91c8a9fa9f7f32
YuleGlycerine <figurefig13@gmail.com>
2024-08-13 11:21:48 +0900

    Feat: SignUpNameInputView 파일 추가

---
+++ Kabinett/Presentation/View/Profile/SignUpNameInputView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpNameInputView.swift
+//  Kabinett
+//
+//  Created by Yule on 8/13/24.
+//
+
+import SwiftUI
+
+struct SignUpNameInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpNameInputView()
+}
