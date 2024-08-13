b62ab1ca06ffb019cad607ec0a8e763c850294c5
YuleGlycerine <figurefig13@gmail.com>
2024-08-13 21:15:04 +0900

    Feat: ProfileView 기본 UI Components 추가

--- Kabinett/Presentation/View/Profile/ProfileView.swift
+++ Kabinett/Presentation/View/Profile/ProfileView.swift
@@ -10,7 +10,18 @@ import SwiftUI
 struct ProfileView: View {
     var body: some View {
         VStack{
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Circle()
+                .foregroundColor(.primary300)
+                .frame(width: 110)
+                .padding(.bottom, -1)
+            Text("User")
+                .fontWeight(.regular)
+                .font(.system(size: 36))
+                .padding(.bottom, 0.1)
+            Text("000-000")
+                .fontWeight(.light)
+                .font(.system(size: 16))
+                .monospaced()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color.background)
