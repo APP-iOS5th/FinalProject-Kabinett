94e48dfb1a13984c013a3bcd94d4bd3f837e7b47
YuleGlycerine <figurefig13@gmail.com>
2024-08-14 23:48:26 +0900

    Feat: AccountSettingView 기본 UI Components 추가

--- Kabinett/Presentation/View/Profile/AccountSettingsView.swift
+++ Kabinett/Presentation/View/Profile/AccountSettingsView.swift
@@ -9,9 +9,37 @@ import SwiftUI
 
 struct AccountSettingsView: View {
     var body: some View {
-        Text("계정 설정")
+        NavigationStack {
+        GeometryReader { geometry in
+                VStack {
+                    HStack{
+                        Text("로그아웃하기")
+                            .fontWeight(.medium)
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .font(.system(size: 19))
+                            .foregroundColor(.contentPrimary)
+                    }
+                    .padding(.top, 10)
+                    .padding(.horizontal, geometry.size.width * 0.06)
+                    Spacer()
+                    Text("회원 탈퇴하기")
+                        .foregroundColor(.alert)
+                        .padding(.bottom, 5)
+                    Text("저장된 데이터가 모두 사라집니다.")
+                        .font(.system(size: 12))
+                        .foregroundColor(.contentSecondary)
+                }
+                .buttonStyle(PlainButtonStyle())
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.background)
+            .navigationTitle("계정 설정")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
+
 
 #Preview {
     AccountSettingsView()
