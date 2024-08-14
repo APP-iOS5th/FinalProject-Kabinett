ab87392b21ef4edb9688a519e4d3c4f8fcb1b250
the-hye <ganzi410@gmail.com>
2024-08-14 09:11:37 +0900

    Fix: Merge Conflict 해결

--- Kabinett/Domain/UseCase/ComponentsUseCase/ComponentsUseCase.swift
+++ Kabinett/Domain/UseCase/ComponentsUseCase/ComponentsUseCase.swift
@@ -8,9 +8,5 @@
 import Foundation
 
 protocol ComponentsUseCase {
-<<<<<<< HEAD
-    func saveLetter(postScript: String?, envelope: String, stamp: String, fromUserId: String, toUserId: String, photoContents: [String], date: Date, isRead: Bool) async -> Result<Void, Error>
-=======
     func saveLetter(postScript: String?, envelope: String, stamp: String, fromUserId: String, toUserId: String, photoContents: [String], date: Date, isRead: Bool) async -> Result<Void, any Error>
->>>>>>> 348edc19f520cca460d1ff70b93953656e618c98
 }
