f3d80eac71373b38038fb11f090b41f1bafe5cde
YuleGlycerine <figurefig13@gmail.com>
2024-08-18 18:43:43 +0900

    Refactor: KabinettNumber 보여주는 로직 수정

--- Kabinett/Presentation/View/Profile/SignUpKabinettNumberSelectView.swift
+++ Kabinett/Presentation/View/Profile/SignUpKabinettNumberSelectView.swift
@@ -24,51 +24,56 @@ struct SignUpKabinettNumberSelectView: View {
                         .padding(.bottom, 15)
                     
                     VStack{
-                        ForEach(0..<3, id: \.self) { index in
-                            let kabinettNumber = viewModel.kabinettNumbers[index]
-                            HStack{
-                                ZStack(alignment: .leading) {
-                                    Capsule()
-                                        .stroke(Color.primary300, lineWidth: 1)
-                                        .background(Capsule().fill(Color.white))
-                                    Text(kabinettNumber)
-                                        .fontWeight(.light)
-                                        .font(.system(size: 20))
-                                        .monospaced()
-                                        .foregroundStyle(.contentPrimary)
-                                        .padding(.leading, 8)
-                                        .padding(10)
-                                }
-                                .frame(width: geometry.size.width * 0.72, height: 54)
-                                .padding(.bottom, 8)
-                                
-                                Button(action: {
-                                    viewModel.selectedKabinettNumber = index
-                                }) {
-                                    ZStack{
-                                        Circle()
-                                            .foregroundColor(viewModel.selectedKabinettNumber == index ? .contentPrimary : .primary300)
-                                            .frame(width: 53)
-                                        Image(systemName: "checkmark")
+                        if viewModel.kabinettNumbers.count >= 3 {
+                            ForEach(0..<3, id: \.self) { index in
+                                let kabinettNumber = viewModel.kabinettNumbers[index]
+                                HStack{
+                                    ZStack(alignment: .leading) {
+                                        Capsule()
+                                            .stroke(Color.primary300, lineWidth: 1)
+                                            .background(Capsule().fill(Color.white))
+                                        Text(kabinettNumber)
                                             .fontWeight(.light)
                                             .font(.system(size: 20))
-                                            .foregroundColor(.white)
+                                            .monospaced()
+                                            .foregroundStyle(.contentPrimary)
+                                            .padding(.leading, 8)
+                                            .padding(10)
                                     }
+                                    .frame(width: geometry.size.width * 0.72, height: 54)
                                     .padding(.bottom, 8)
+                                    
+                                    Button(action: {
+                                        viewModel.selectedKabinettNumber = index
+                                    }) {
+                                        ZStack{
+                                            Circle()
+                                                .foregroundColor(viewModel.selectedKabinettNumber == index ? .contentPrimary : .primary300)
+                                                .frame(width: 53)
+                                            Image(systemName: "checkmark")
+                                                .fontWeight(.light)
+                                                .font(.system(size: 20))
+                                                .foregroundColor(.white)
+                                        }
+                                        .padding(.bottom, 8)
+                                    }
                                 }
                             }
+                        } else {
+                            Text("No available Kabinett numbers.")
+                                .foregroundColor(.alert)
                         }
                     }
                     .padding(.leading, geometry.size.width * 0.06)
                     Spacer()
                     Button(action: {
-//                        if viewModel.selectedKabinettNumber != nil {
-//                            shouldNavigatedToProfile = true
-                            print("UserName: \(viewModel.userName)")
-                            if let selectedNumber = viewModel.selectedKabinettNumber {
-                                print("Selected Kabinett Number: \(viewModel.kabinettNumbers[selectedNumber])")
-                            }
-//                        }
+                        //                        if viewModel.selectedKabinettNumber != nil {
+                        //                            shouldNavigatedToProfile = true
+                        print("UserName: \(viewModel.userName)")
+                        if let selectedNumber = viewModel.selectedKabinettNumber {
+                            print("Selected Kabinett Number: \(viewModel.kabinettNumbers[selectedNumber])")
+                        }
+                        //                        }
                     }) {
                         Text("시작하기")
                             .fontWeight(.medium)
@@ -80,13 +85,16 @@ struct SignUpKabinettNumberSelectView: View {
                     }
                     .disabled(viewModel.selectedKabinettNumber == nil)
                     .padding(.horizontal, geometry.size.width * 0.06)
-//                    .navigationDestination(isPresented: $shouldNavigatedToProfile) {
-//                        ProfileView(userName: userName, userNumber: kabinettNumbers[selectedKabinettNumber ?? 0])
-//                    }
+                    //                    .navigationDestination(isPresented: $shouldNavigatedToProfile) {
+                    //                        ProfileView(userName: userName, userNumber: kabinettNumbers[selectedKabinettNumber ?? 0])
+                    //                    }
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                 .background(Color.background)
             }
+        }
+        .task {
+            await viewModel.getNumbers()
         }
     }
 }
