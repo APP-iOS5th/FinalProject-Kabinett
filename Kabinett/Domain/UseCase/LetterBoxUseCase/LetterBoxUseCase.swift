a4833285741472d366ec8f447c46e521ee737326
the-hye <ganzi410@gmail.com>
2024-08-19 10:41:57 +0900

    Fix: LetterBoxUseCase getIsRead return 값 수정

--- Kabinett/Domain/UseCase/LetterBoxUseCase/LetterBoxUseCase.swift
+++ Kabinett/Domain/UseCase/LetterBoxUseCase/LetterBoxUseCase.swift
@@ -10,7 +10,7 @@ import Foundation
 protocol LetterBoxUseCase {
     func getLetterBoxLetters(userId: String) async -> Result<[LetterType: [Letter]], any Error>
     func getLetterBoxDetailLetters(userId: String, letterType: LetterType) async -> Result<[Letter], any Error>
-    func getIsRead(userId: String, letterType: LetterType) async -> Result<Int, any Error>
+    func getIsRead(userId: String) async -> Result<[LetterType: Int], any Error>
 }
 
 enum LetterType {
