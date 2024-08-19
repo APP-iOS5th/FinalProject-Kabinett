d4e3c5e17a7d8e6f9cba76f0e34553c33e267ea7
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-08-19 14:36:25 +0900

    Feat: 사진 미리보기 ImagePreview에서 이미지 클릭 시 자세히 볼 수 있는 ImageDetailView 추가

---
+++ Kabinett/Presentation/View/Components/ImageDetailView.swift
@@ -0,0 +1,46 @@
+//
+//  ImageDetailView.swift
+//  Kabinett
+//
+//  Created by 김정우 on 8/19/24.
+//
+
+import SwiftUI
+
+struct ImageDetailView: View {
+    let images: [IdentifiableImage]
+    @State private var currentIndex = 0
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                TabView(selection: $currentIndex) {
+                    ForEach(images.indices, id: \.self) { index in
+                        Image(uiImage: images[index].image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .tag(index)
+                    }
+                }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                
+                HStack {
+                    Button(action: { currentIndex = max(currentIndex - 1, 0) }) {
+                        Image(systemName: "chevron.left")
+                            .foregroundColor(.white)
+                    }
+                    .opacity(currentIndex > 0 ? 1 : 0)
+                    
+                    Spacer()
+                    
+                    Button(action: { currentIndex = min(currentIndex + 1, images.count - 1) }) {
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.white)
+                    }
+                    .opacity(currentIndex < images.count - 1 ? 1 : 0)
+                }
+                .padding()
+            }
+        }
+    }
+}
