8aa8401e5cb019e503d3ea44f84a1f5f24e12362
uunwon <chea2089@naver.com>
2024-08-19 13:07:41 +0900

    Fix: ToastView Style 변경

--- Kabinett/Presentation/View/LetterBox/ToastView.swift
+++ Kabinett/Presentation/View/LetterBox/ToastView.swift
@@ -15,20 +15,20 @@ struct ToastView: View {
         ZStack {
             Rectangle()
                 .fill(.clear)
-                .background(.primary300.opacity(0.8))
-                .background(.ultraThinMaterial)
-                .frame(height: 45)
+                .background(.primary900)
+                .frame(height: 47)
                 .cornerRadius(30)
                 .padding(.horizontal, horizontalPadding)
                 .padding(.bottom, 20)
             
             Text(message)
-                .font(.system(size: 14))
+                .font(.system(size: 15, weight: .heavy))
+                .foregroundStyle(.white)
                 .padding(.bottom, 20)
         }
     }
 }
 
 #Preview {
-    ToastView(message: "편지가 도착했습니다.", horizontalPadding: 50)
+    ToastView(message: "편지가 도착했어요.", horizontalPadding: 50)
 }
