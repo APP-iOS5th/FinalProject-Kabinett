c512e241d4c316123fb9524353de1cfe0b062d7a
song <nadana0929@gmail.com>
2024-08-21 17:11:02 +0900

    Fix: 에러 고침 ..

--- Kabinett/Presentation/View/LetterWrite/OpenSourceLicenseModalView.swift
+++ Kabinett/Presentation/View/LetterWrite/OpenSourceLicenseModalView.swift
@@ -40,7 +40,3 @@ struct OpenSourceLicenseModalView: View {
         }
     }
 }
-
-#Preview {
-    OpenSourceLicenseModalView()
-}
--- Kabinett/Presentation/ViewModel/LetterWrite/FontSelectionViewModel.swift
+++ Kabinett/Presentation/ViewModel/LetterWrite/FontSelectionViewModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftUI
+import UIKit
 
 struct Fonts: Identifiable {
     let id = UUID()
