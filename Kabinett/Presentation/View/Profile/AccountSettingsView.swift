efe0eadd03aa38090a35786e0a77bfa311d82e48
YuleGlycerine <figurefig13@gmail.com>
2024-08-22 17:23:56 +0900

    Feat: AccountSettingsView 로그아웃, 회원탈퇴 버튼 Hit Area 확장

--- Kabinett/Presentation/View/Profile/AccountSettingsView.swift
+++ Kabinett/Presentation/View/Profile/AccountSettingsView.swift
@@ -12,29 +12,45 @@ struct AccountSettingsView: View {
     
     var body: some View {
         NavigationStack {
-        GeometryReader { geometry in
+            GeometryReader { geometry in
                 VStack {
-                    HStack{
-                        Text("로그아웃하기")
-                            .fontWeight(.medium)
-                            .font(.system(size: 17))
-                            .foregroundColor(.contentPrimary)
-                        Spacer()
-                        Image(systemName: "chevron.right")
-                            .font(.system(size: 19))
-                            .foregroundColor(.contentPrimary)
+                    Button(action: {
+                        print("로그아웃")
+                    }) {
+                        HStack{
+                            Text("로그아웃하기")
+                                .fontWeight(.medium)
+                                .font(.system(size: 17))
+                                .foregroundColor(.contentPrimary)
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .font(.system(size: 19))
+                                .foregroundColor(.contentPrimary)
+                        }
+                        .padding(.top, 20)
+                        .padding(.horizontal, geometry.size.width * 0.06)
                     }
-                    .padding(.top, 20)
-                    .padding(.horizontal, geometry.size.width * 0.06)
+                    .buttonStyle(PlainButtonStyle())
+                    .contentShape(Rectangle())
+                    
                     Spacer()
-                    Text("회원 탈퇴하기")
-                        .foregroundColor(.alert)
-                        .padding(.bottom, 5)
-                    Text("저장된 데이터가 모두 사라집니다.")
-                        .font(.system(size: 12))
-                        .foregroundColor(.contentSecondary)
+                    
+                    Button(action: {
+                        print("회원탈퇴")
+                    }) {
+                        VStack {
+                            Text("회원 탈퇴하기")
+                                .foregroundColor(.alert)
+                                .padding(.bottom, 5)
+                            Text("저장된 데이터가 모두 사라집니다.")
+                                .font(.system(size: 12))
+                                .foregroundColor(.contentSecondary)
+                        }
+                        .padding(.horizontal, geometry.size.width * 0.06)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .contentShape(Rectangle())
                 }
-                .buttonStyle(PlainButtonStyle())
                 .navigationBarBackButtonHidden(true)
                 .toolbar {
                     ToolbarItem(placement: .navigationBarLeading) {
@@ -57,7 +73,6 @@ struct AccountSettingsView: View {
         }
     }
 }
-
 
 #Preview {
     AccountSettingsView()
