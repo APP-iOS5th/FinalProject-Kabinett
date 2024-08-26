b077f476aebf3cfd4a8a3eae70d3715a6fa4ce83
uunwon <chea2089@naver.com>
2024-08-26 11:03:11 +0900

    Feat: 편지 삭제 ViewModel 구현

--- Kabinett/Presentation/ViewModel/LetterBox/LetterBoxDetailViewModel.swift
+++ Kabinett/Presentation/ViewModel/LetterBox/LetterBoxDetailViewModel.swift
@@ -15,6 +15,7 @@ class LetterBoxDetailViewModel: ObservableObject {
     }
     
     @Published var letterBoxDetailLetters: [Letter] = []
+    @Published var isRemoveLetter: Bool = false
     
     @Published var errorMessage: String?
     
@@ -24,6 +25,18 @@ class LetterBoxDetailViewModel: ObservableObject {
             switch result {
             case .success(let letterArray):
                 self.letterBoxDetailLetters = letterArray
+            case .failure(let error):
+                self.errorMessage = error.localizedDescription
+            }
+        }
+    }
+    
+    func removeLetterBoxDetailLetter(for userId: String, letterId: String, letterType: LetterType) {
+        Task { @MainActor in
+            let result = await letterBoxUseCase.removeLetter(userId: userId, letterId: letterId, letterType: letterType)
+            switch result {
+            case .success(let isRemove):
+                self.isRemoveLetter = isRemove
             case .failure(let error):
                 self.errorMessage = error.localizedDescription
             }
