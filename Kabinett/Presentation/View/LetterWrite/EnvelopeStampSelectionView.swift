7ebf9e74ba097d4c2c19e1602b98d67f2b0f247c
song <nadana0929@gmail.com>
2024-08-28 14:10:29 +0900

    Feat: 편지봉투, 우표 선택하는 창과 탭 바 구현 + 선택된 봉투와 우표 만들어놓은 뷰에 적용

--- Kabinett/Presentation/View/LetterWrite/EnvelopeStampSelectionView.swift
+++ Kabinett/Presentation/View/LetterWrite/EnvelopeStampSelectionView.swift
@@ -12,7 +12,15 @@ struct EnvelopeStampSelectionView: View {
     @Binding var letterContent: LetterWriteViewModel
     @StateObject private var viewModel = EnvelopeStampSelectionViewModal()
     
-    @State var text: String = ""
+    @State private var text: String = ""
+    @State private var envelopeImageUrl: String
+    @State private var stampImageUrl: String
+    
+    init(letterContent: Binding<LetterWriteViewModel>) {
+        _letterContent = letterContent
+        _envelopeImageUrl = State(initialValue: letterContent.wrappedValue.envelopeImageUrlString)
+        _stampImageUrl = State(initialValue: letterContent.wrappedValue.stampImageUrlString)
+    }
     
     var body: some View {
         ZStack {
@@ -25,10 +33,14 @@ struct EnvelopeStampSelectionView: View {
                     
                     VStack {
                         ZStack(alignment: .topLeading) {
-                            Rectangle() // 편지지 이미지로 변경 할 예정
-                                .fill(Color.white)
+                            KFImage(URL(string: envelopeImageUrl))
+                                .resizable()
                                 .shadow(radius: 5, x: 5, y: 5)
                                 .frame(width: .infinity, height: .infinity)
+                                .onAppear {
+                                    envelopeImageUrl = viewModel.dummyEnvelopes[0]
+                                    letterContent.envelopeImageUrlString = envelopeImageUrl
+                                }
                             
                             VStack {
                                 HStack(alignment: .top) {
@@ -43,11 +55,15 @@ struct EnvelopeStampSelectionView: View {
                                     
                                     Spacer()
                                     
-                                    Rectangle() // 우표 이미지로 변경 할 예정
-                                        .fill(Color.red)
+                                    KFImage(URL(string: stampImageUrl))
+                                        .resizable()
                                         .aspectRatio(9/9.7, contentMode: .fit)
                                         .frame(width: geometry.size.width * 0.1, height: .infinity)
                                         .padding(.trailing, 25)
+                                        .onAppear {
+                                            stampImageUrl = viewModel.dummyStamps[0]
+                                            letterContent.stampImageUrlString = stampImageUrl
+                                        }
                                 }
                                 .padding(.top, 25)
                                 
@@ -73,7 +89,7 @@ struct EnvelopeStampSelectionView: View {
                                 }
                                 .padding(.bottom, 30)
                             }
-
+                            
                         }
                         .aspectRatio(9/4, contentMode: .fit)
                         .padding(.bottom, 50)
@@ -82,7 +98,7 @@ struct EnvelopeStampSelectionView: View {
                             Text("봉투에 적을 내용")
                                 .font(.system(size: 13))
                                 .padding(.bottom, 1)
-                            TextField("최대 15자까지 적을 수 있습니다 ~", text: $text)
+                            TextField("최대 15글자를 적을 수 있어요.", text: $text)
                                 .maxLength(text: $text, 15)
                                 .padding(.leading, 6)
                                 .font(.system(size: 14))
@@ -92,6 +108,7 @@ struct EnvelopeStampSelectionView: View {
                         }
                         .padding(.bottom, 30)
                     }
+                    SelectionTabView(letterContent: $letterContent, envelopeImageUrl: $envelopeImageUrl, stampImageUrl: $stampImageUrl)
                 }
                 .padding(.horizontal, UIScreen.main.bounds.width * 0.06)
             }
@@ -101,6 +118,116 @@ struct EnvelopeStampSelectionView: View {
     }
 }
 
-#Preview {
-    ModalTestView()
+struct EnvelopeCell: View {
+    @Binding var letterContent: LetterWriteViewModel
+    @Binding var envelopeImageUrl: String
+    @StateObject private var viewModel = EnvelopeStampSelectionViewModal()
+    
+    var body: some View {
+        ZStack {
+            Color("Background").ignoresSafeArea()
+            
+            VStack {
+                List {
+                    ForEach(0..<viewModel.envelopeNumberOfRows, id: \.self) { rowIndex in
+                        HStack {
+                            ForEach(0..<2, id: \.self) { columnIndex in
+                                let index = viewModel.envelopeIndex(row: rowIndex, column: columnIndex)
+                                
+                                ZStack(alignment: .topTrailing) {
+                                    KFImage(URL(string: viewModel.dummyEnvelopes[index]))
+                                        .placeholder {
+                                            Image(systemName: "arrow.down.circle.dotted")
+                                        }
+                                        .resizable()
+                                        .aspectRatio(9/4, contentMode: .fit)
+                                        .padding(10)
+                                        .shadow(radius: 5, x: 5, y: 5)
+                                        .onTapGesture {
+                                            viewModel.envelopeSelectStationery(coordinates: (rowIndex, columnIndex))
+                                            envelopeImageUrl = viewModel.dummyEnvelopes[index]
+                                            letterContent.envelopeImageUrlString = viewModel.dummyEnvelopes[index]
+                                        }
+                                    
+                                    if viewModel.isEnvelopeSelected(coordinates: (rowIndex, columnIndex)) {
+                                        Image("checked")
+                                            .resizable()
+                                            .frame(width: 27, height: 27)
+                                            .padding([.top, .trailing], 20)
+                                            .onAppear {
+                                                letterContent.envelopeImageUrlString = viewModel.dummyEnvelopes[viewModel.envelopeIndex(row: rowIndex, column: columnIndex)]
+                                            }
+                                    }
+                                }
+                            }
+                        }
+                        .listRowBackground(Color.clear)
+                        .listRowSeparator(.hidden)
+                        .listRowInsets(EdgeInsets())
+                        .padding(.top, 10)
+                    }
+                }
+                .listStyle(.plain)
+                .padding(.leading, -10)
+                .padding(.trailing, -5)
+            }
+        }
+    }
+}
+
+struct StampCell: View {
+    @Binding var letterContent: LetterWriteViewModel
+    @Binding var stampImageUrl: String
+    @StateObject private var viewModel = EnvelopeStampSelectionViewModal()
+    
+    var body: some View {
+        ZStack {
+            Color("Background").ignoresSafeArea()
+            
+            VStack {
+                List {
+                    ForEach(0..<viewModel.stampNumberOfRows, id: \.self) { rowIndex in
+                        HStack {
+                            ForEach(0..<3, id: \.self) { columnIndex in
+                                let index = viewModel.stampIndex(row: rowIndex, column: columnIndex)
+                                
+                                ZStack(alignment: .topTrailing) {
+                                    KFImage(URL(string: viewModel.dummyStamps[index]))
+                                        .placeholder {
+                                            Image(systemName: "arrow.down.circle.dotted")
+                                        }
+                                        .resizable()
+                                        .aspectRatio(9/9.7, contentMode: .fit)
+                                        .padding(10)
+                                        .shadow(radius: 5, x: 5, y: 5)
+                                        .onTapGesture {
+                                            viewModel.stampSelectStationery(coordinates: (rowIndex, columnIndex))
+                                            stampImageUrl = viewModel.dummyStamps[index]
+                                            letterContent.stampImageUrlString = viewModel.dummyStamps[index]
+                                        }
+                                    
+                                    if viewModel.isStampSelected(coordinates: (rowIndex, columnIndex)) {
+                                        Image("checked")
+                                            .resizable()
+                                            .frame(width: 27, height: 27)
+                                            .padding([.top, .trailing], 20)
+                                            .onAppear {
+                                                letterContent.stampImageUrlString = viewModel.dummyStamps[viewModel.stampIndex(row: rowIndex, column: columnIndex)]
+                                            }
+                                    }
+                                }
+                            }
+                        }
+                        .listRowBackground(Color.clear)
+                        .listRowSeparator(.hidden)
+                        .listRowInsets(EdgeInsets())
+                        .padding(.top, 10)
+                    }
+                }
+                .listStyle(.plain)
+                .padding(.leading, -10)
+                .padding(.trailing, -5)
+            }
+        }
+    }
 }
