9f0f0fc747670bf4f31d2dfad9cbba2a11ac8bf9
uunwon <chea2089@naver.com>
2024-08-28 10:07:01 +0900

    FIx: 함수 위치 변경

--- Kabinett/Presentation/ViewModel/LetterBox/LetterBoxViewModel.swift
+++ Kabinett/Presentation/ViewModel/LetterBox/LetterBoxViewModel.swift
@@ -50,6 +50,24 @@ class LetterBoxViewModel: ObservableObject {
     func getIsReadLetters(for type: LetterType) -> Int {
         return isReadLetters[type] ?? 0
     }
+    
+    func calculateOffsetAndRotation(for index: Int, totalCount: Int) -> (xOffset: CGFloat, yOffset: CGFloat, rotation: Double) {
+        switch totalCount {
+        case 1:
+            return (xOffset: CGFloat(-6.5), yOffset: CGFloat(-1.5), rotation: Double(-1.5))
+        case 2:
+            let xOffset = index == 0 ? -7 : 6
+            let yOffset = index == 0 ? -10 : -2
+            let rotation = index == 0 ? -1 : 0
+            return (xOffset: CGFloat(xOffset), yOffset: CGFloat(yOffset), rotation: Double(rotation))
+        case 3:
+            let xOffset = [-12, -5, 12][index]
+            let yOffset = [-3, -12, -2][index]
+            return (xOffset: CGFloat(xOffset), yOffset: CGFloat(yOffset), rotation: 0)
+        default:
+            return (xOffset: 0, yOffset: 0, rotation: 0)
+        }
+    }
 }
 
 extension LetterType {
