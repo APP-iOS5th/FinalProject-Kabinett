290ee216e603598bdb327c0d39b108f88d665237
YuleGlycerine <figurefig13@gmail.com>
2024-08-30 11:21:49 +0900

    Feat: 유저 로그인 상태에 따른 프로필 탭 화면 분기

--- Kabinett/Presentation/View/Profile/ProfileView.swift
+++ Kabinett/Presentation/View/Profile/ProfileView.swift
@@ -8,53 +8,67 @@
 import SwiftUI
 
 struct ProfileView: View {
-    @StateObject var profileViewModel: ProfileSettingsViewModel
+    @StateObject var profileViewModel = ProfileSettingsViewModel(profileUseCase: ProfileUseCaseStub())
     
     var body: some View {
         GeometryReader { geometry in
             NavigationStack {
-                VStack{
-                    if let image = profileViewModel.profileImage {
-                        Image(uiImage: image)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width:110, height: 110)
-                            .clipShape(Circle())
-                            .padding(.bottom, -1)
+                Group {
+                    if profileViewModel.shouldNavigateToLogin {
+                        LoginView()
                     } else {
-                        Circle()
-                            .foregroundColor(.primary300)
-                            .frame(width: 110, height: 110)
-                            .padding(.bottom, -1)
-                    }
-                    
-                    Text(profileViewModel.userName)
-                        .fontWeight(.regular)
-                        .font(.system(size: 36))
-                        .padding(.bottom, 0.1)
-                    Text(profileViewModel.formattedKabinettNumber)
-                        .fontWeight(.light)
-                        .font(.system(size: 16))
-                        .monospaced()
-                }
-                .padding(.horizontal, geometry.size.width * 0.06)
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(Color.background)
-                .navigationBarBackButtonHidden()
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        NavigationLink(destination: SettingsView(profileViewModel: profileViewModel)) {
-                            Image(systemName: "gearshape")
-                                .fontWeight(.medium)
-                                .font(.system(size: 19))
-                                .foregroundColor(.contentPrimary)
+                        VStack {
+                            if let image = profileViewModel.profileImage {
+                                Image(uiImage: image)
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width:110, height: 110)
+                                    .clipShape(Circle())
+                                    .padding(.bottom, -1)
+                            } else {
+                                Circle()
+                                    .foregroundColor(.primary300)
+                                    .frame(width: 110, height: 110)
+                                    .padding(.bottom, -1)
+                            }
+                            
+                            Text(profileViewModel.userName)
+                                .fontWeight(.regular)
+                                .font(.system(size: 36))
+                                .padding(.bottom, 0.1)
+                            Text(profileViewModel.formattedKabinettNumber)
+                                .fontWeight(.light)
+                                .font(.system(size: 16))
+                                .monospaced()
+                        }
+                        .padding(.horizontal, geometry.size.width * 0.06)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(Color.background)
+                        .toolbar {
+                            ToolbarItem(placement: .navigationBarTrailing) {
+                                NavigationLink(destination: SettingsView(profileViewModel: profileViewModel)) {
+                                    Image(systemName: "gearshape")
+                                        .fontWeight(.medium)
+                                        .font(.system(size: 19))
+                                        .foregroundColor(.contentPrimary)
+                                }
+                            }
                         }
                     }
                 }
+                .navigationBarBackButtonHidden()
+            }
+        }
+        .onAppear {
+            Task {
+                await profileViewModel.checkUserStatus()
+                print("User status after check: \(profileViewModel.userStatus))")
+                print("Should navigate to login: \(profileViewModel.shouldNavigateToLogin)")
             }
         }
     }
 }
+
 #Preview {
     ProfileView(profileViewModel: ProfileSettingsViewModel(profileUseCase: ProfileUseCaseStub()))
 }
