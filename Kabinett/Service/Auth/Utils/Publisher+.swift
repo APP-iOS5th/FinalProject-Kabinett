2663a75b7f8f89232c4b3f0707ada396fe471938
jinwoong <baseball0020@gmail.com>
2024-08-30 13:18:38 +0900

    Feat: Combine `asyncMap` 오퍼레이터 추가

---
+++ Kabinett/Service/Auth/Utils/Publisher+.swift
@@ -0,0 +1,23 @@
+//
+//  Publisher+.swift
+//  Kabinett
+//
+//  Created by jinwoong Kim on 8/30/24.
+//
+
+import Combine
+
+extension Publisher {
+    func asyncMap<T>(
+        _ transform: @escaping (Output) async -> T
+    ) -> Publishers.FlatMap<Future<T, Never>, Self> {
+        flatMap { value in
+            Future { promise in
+                Task {
+                    let output = await transform(value)
+                    promise(.success(output))
+                }
+            }
+        }
+    }
+}
