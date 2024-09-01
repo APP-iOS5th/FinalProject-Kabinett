ab0a74564f6dc780b76e5af31eaa47257377afaa
jinwoong <baseball0020@gmail.com>
2024-09-01 14:48:19 +0900

    Refactor: 계정 연결의 결과에 따른 분기 처리
    - 계정 연결 성공시, 이는 새로이 가입하는 유저라는 의미
    - 계정 연결 실패하고 등록된 유저라면, 단순히 로그인 후, 해당 유저 정보를 반환

--- Kabinett/Service/Auth/AuthManager.swift
+++ Kabinett/Service/Auth/AuthManager.swift
@@ -11,6 +11,11 @@ import Combine
 import os
 
 final class AuthManager {
+    enum UserInfo {
+        case newUser
+        case existingUser(User?)
+    }
+    
     private let logger: Logger
     private var currentUserSubject: CurrentValueSubject<User?, Never> = .init(nil)
     private let writerManager: FirestoreWriterManager
@@ -49,18 +54,24 @@ final class AuthManager {
         }
     }
     
+    // TODO: Change Method name
+    // TODO: Add error handling
     func linkAccount(
-        with credential: OAuthCredential
-    ) async -> Bool {
+        with idTokenString: String
+    ) async -> UserInfo {
+        let credential = OAuthProvider.credential(
+            providerID: .apple,
+            accessToken: idTokenString
+        )
         do {
             if let user = Auth.auth().currentUser {
                 let result = try await user.link(with: credential)
                 currentUserSubject.send(result.user)
                 
-                return true
+                return .newUser
             } else {
                 logger.warning("Attempting to linking account without current user is not allowed.")
-                return false
+                return .newUser
             }
         } catch {
             let error = error as NSError
@@ -69,13 +80,13 @@ final class AuthManager {
             if code == .credentialAlreadyInUse {
                 logger.debug("This credential already in use, delete current user and retry signing.")
                 deleteAccount()
-                await signInWith(credential: credential)
+                let user = await signInWith(credential: credential)
                 
-                return true
+                return .existingUser(user)
             } else {
                 logger.debug("Linking Error: \(error.localizedDescription)")
                 
-                return false
+                return .newUser
             }
         }
     }
