b93a57268c3c069f3ca8cc68c189de951a1cd2fd
song <nadana0929@gmail.com>
2024-09-02 15:43:29 +0900

    Fix: 에뮬레이터 주석처리

--- Kabinett/Application/KabinettApp.swift
+++ Kabinett/Application/KabinettApp.swift
@@ -15,19 +15,19 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         // Uncomment these blocks to use Firebase Emulator Suite
-        #if DEBUG
-        // Firebase Authenticate Emulator
-        Auth.auth().useEmulator(withHost:"localhost", port:9099)
-        
-        // Firebase Storage Emulator
-        Storage.storage().useEmulator(withHost: "localhost", port: 9199)
-        
-        // Firebaes Firestore Emulator
-        let settings = Firestore.firestore().settings
-        settings.host = "localhost:8080"
-        settings.isSSLEnabled = false
-        Firestore.firestore().settings = settings
-        #endif
+//        #if DEBUG
+//        // Firebase Authenticate Emulator
+//        Auth.auth().useEmulator(withHost:"localhost", port:9099)
+//        
+//        // Firebase Storage Emulator
+//        Storage.storage().useEmulator(withHost: "localhost", port: 9199)
+//        
+//        // Firebaes Firestore Emulator
+//        let settings = Firestore.firestore().settings
+//        settings.host = "localhost:8080"
+//        settings.isSSLEnabled = false
+//        Firestore.firestore().settings = settings
+//        #endif
         
         return true
     }
