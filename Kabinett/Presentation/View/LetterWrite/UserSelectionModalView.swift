e172187c5ecb615236386af65b67db0546b7db63
song <nadana0929@gmail.com>
2024-09-04 21:47:29 +0900

    Fix: 유저 업데이트 될 수 있게 코드 수정 (피드백부분)

--- Kabinett/Presentation/View/LetterWrite/UserSelectionModalView.swift
+++ Kabinett/Presentation/View/LetterWrite/UserSelectionModalView.swift
@@ -108,15 +108,7 @@ struct FormToUser: View {
                 .clipShape(Capsule())
         }
         .padding(.top, 24)
-        .onAppear {
-            /// 현재 이부분은 화면이 나타나면 실행되는 부분입니다.
-            /// 하지만 onAppear는 모달로 띄운 뷰(로그인뷰)를 닫고 돌아왔을 때는
-            /// 실행되지 않아요.
-            /// 그 말은, 이 화면은 아예 뒤로가기를 눌러서 껏다가 키는게 아니라면
-            /// `letterContent`는 로그인을 완료하든 아니든 그대로일 것 입니다.
-            /// 이 부분을 수정해보시면 좋을 것 같아요.
-            /// 아울러, 103줄은 화면 갱신을 위해 viewModel을 사용하는 코드로
-            /// 변경하였습니다.
+        .onChange(of: viewModel.userKabiNumber) {
             letterContent.fromUserId = viewModel.fromUser?.id
             letterContent.fromUserName = viewModel.fromUser?.name ?? ""
             letterContent.fromUserKabinettNumber = viewModel.fromUser?.kabinettNumber
