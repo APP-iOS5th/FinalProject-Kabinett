62445c100fe2c1eae20ef8c604fa898022882cd6
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-09-05 13:59:56 +0900

    Fix: tabItem 위치수정

--- Kabinett/Presentation/View/Components/CustomTabView/CustomTabBar.swift
+++ Kabinett/Presentation/View/Components/CustomTabView/CustomTabBar.swift
@@ -9,22 +9,31 @@ import SwiftUI
 
 struct CustomTabBar: View {
     @EnvironmentObject var viewModel: CustomTabViewModel
-    
+
     var body: some View {
         GeometryReader { geometry in
             HStack(spacing: 0) {
                 tabItem(image: viewModel.envelopeImage, tag: 0)
                     .frame(width: geometry.size.width / 3)
-                
                 tabItem(image: viewModel.plusImage, tag: 1)
                     .frame(width: geometry.size.width / 3)
-                
                 tabItem(image: viewModel.profileImage, tag: 2)
                     .frame(width: geometry.size.width / 3)
             }
             .frame(width: geometry.size.width, height: 44)
             .background(Color.clear)
-            .position(x: geometry.size.width / 2, y: geometry.size.height - 22)
+            .position(x: geometry.size.width / 2, y: calculateYPosition(geometry: geometry))
+        }
+    }
+    
+    private func calculateYPosition(geometry: GeometryProxy) -> CGFloat {
+        let basePosition: CGFloat = geometry.size.height - 22
+        let bottomSafeAreaHeight = geometry.safeAreaInsets.bottom
+        
+        if bottomSafeAreaHeight > 0 {
+            return basePosition - bottomSafeAreaHeight + 15
+        } else {
+            return basePosition - 10
         }
     }
     
