cc199528261f6cc4b0f84047ff0dc36ee76c53fb
song <nadana0929@gmail.com>
2024-09-06 13:16:01 +0900

    Fix: Padding - 는 화면이 겹칠 수 있어서 Spacer()로 변경

--- Kabinett/Presentation/View/LetterWrite/ContentWriteView.swift
+++ Kabinett/Presentation/View/LetterWrite/ContentWriteView.swift
@@ -27,7 +27,7 @@ struct ContentWriteView: View {
                     .padding(.horizontal, UIScreen.main.bounds.width * 0.06)
                 
                 ScrollableLetterView(letterContent: $letterContent)
-                    .padding(.top, -25)
+                Spacer()
             }
         }
         .navigationBarBackButtonHidden()
