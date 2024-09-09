76308d245d0a02ccba8e2fc6416d25e8cad2a92c
YuleGlycerine <figurefig13@gmail.com>
2024-09-09 11:55:51 +0900

    Feat: 카비넷 넘버 받아올 때 프로그레스뷰

--- Kabinett/Presentation/View/SignUp/SignUpKabinettNumberSelectView.swift
+++ Kabinett/Presentation/View/SignUp/SignUpKabinettNumberSelectView.swift
@@ -10,115 +10,120 @@ import SwiftUI
 struct SignUpKabinettNumberSelectView: View {
     @EnvironmentObject var viewModel: SignUpViewModel
     @Environment(\.dismiss) var dismiss
+    let horizontalPadding: CGFloat = UIScreen.main.bounds.width * 0.06
     
     var body: some View {
-        GeometryReader { geometry in
-            VStack(alignment: .leading) {
+        ZStack {
+            Color.background.ignoresSafeArea(.all)
+            VStack(alignment: .leading){
                 Spacer()
-                Text("마음에 드는 카비넷 번호를 선택해주세요.")
-                    .fontWeight(.regular)
-                    .font(.system(size: 16))
-                    .foregroundStyle(.contentPrimary)
-                    .padding(.leading, geometry.size.width * 0.06)
-                    .padding(.bottom, 15)
-                
-                VStack{
-                    if viewModel.availablekabinettNumbers.count >= 3 {
-                        ForEach(0..<3, id: \.self) { index in
-                            let availablekabinettNumber = viewModel.availablekabinettNumbers[index]
-                            HStack{
-                                ZStack(alignment: .leading) {
-                                    Capsule()
-                                        .stroke(Color.primary300, lineWidth: 1)
-                                        .background(Capsule().fill(Color.white))
-                                    Text(availablekabinettNumber)
-                                        .fontWeight(.light)
-                                        .font(.system(size: 20))
-                                        .monospaced()
-                                        .foregroundStyle(.contentPrimary)
-                                        .padding(.leading, 8)
-                                        .padding(10)
-                                }
-                                .frame(width: geometry.size.width * 0.72, height: 54)
-                                .padding(.bottom, 8)
-                                
-                                Button(action: {
-                                    viewModel.selectedKabinettNumber = index
-                                }) {
-                                    ZStack{
-                                        Circle()
-                                            .foregroundColor(viewModel.selectedKabinettNumber == index ? .contentPrimary : .primary300)
-                                            .frame(width: 53)
-                                        Image(systemName: "checkmark")
+                if viewModel.isLoading {
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle())
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                } else {
+                    VStack(alignment: .leading) {
+                        Text("마음에 드는 카비넷 번호를 선택해주세요.")
+                            .fontWeight(.regular)
+                            .font(.system(size: 16))
+                            .foregroundStyle(.contentPrimary)
+                            .padding(.bottom, 15)
+                        
+                        if viewModel.availablekabinettNumbers.count >= 3 {
+                            ForEach(0..<3, id: \.self) { index in
+                                let availablekabinettNumber = viewModel.availablekabinettNumbers[index]
+                                HStack{
+                                    ZStack(alignment: .leading) {
+                                        Capsule()
+                                            .stroke(Color.primary300, lineWidth: 1)
+                                            .background(Capsule().fill(Color.white))
+                                        Text(availablekabinettNumber)
                                             .fontWeight(.light)
                                             .font(.system(size: 20))
-                                            .foregroundColor(.white)
+                                            .monospaced()
+                                            .foregroundStyle(.contentPrimary)
+                                            .padding(.leading, 8)
+                                            .padding(10)
                                     }
+                                    .frame(width: UIScreen.main.bounds.width * 0.72, height: 54)
                                     .padding(.bottom, 8)
+                                    
+                                    Button(action: {
+                                        viewModel.selectedKabinettNumber = index
+                                    }) {
+                                        ZStack{
+                                            Circle()
+                                                .foregroundColor(viewModel.selectedKabinettNumber == index ? .contentPrimary : .primary300)
+                                                .frame(width: 53)
+                                            Image(systemName: "checkmark")
+                                                .fontWeight(.light)
+                                                .font(.system(size: 20))
+                                                .foregroundColor(.white)
+                                        }
+                                        .padding(.bottom, 8)
+                                    }
+                                }
+                            }
+                        } else {
+                            Text("No available Kabinett numbers.")
+                                .foregroundColor(.alert)
+                        }
+                    }
+                    .padding(.leading, horizontalPadding)
+                    Spacer()
+                    Button(action: {
+                        Task {
+                            if let selectedIndex = viewModel.selectedKabinettNumber {
+                                let selectedKabinettNumber = viewModel.availablekabinettNumbers[selectedIndex]
+                                
+                                let success = await viewModel.startLoginUser(
+                                    with: viewModel.userName,
+                                    kabinettNumber: selectedKabinettNumber
+                                )
+                                
+                                if success {
+                                    viewModel.showSignUpFlow = false
+                                } else {
+                                    viewModel.signUpError = "회원 가입에 실패했어요. 다시 시도해주세요."
+                                    viewModel.showAlert = true
                                 }
                             }
                         }
-                    } else {
-                        Text("No available Kabinett numbers.")
-                            .foregroundColor(.alert)
+                    }) {
+                        Text("시작하기")
+                            .fontWeight(.semibold)
+                            .font(.system(size: 16))
+                            .foregroundColor(.white)
+                            .frame(width: UIScreen.main.bounds.width * 0.86, height: 56)
+                            .background(RoundedRectangle(cornerRadius: 16)
+                                .fill(viewModel.selectedKabinettNumber != nil ? Color.primary900 : Color.primary300))
                     }
-                }
-                .padding(.leading, geometry.size.width * 0.06)
-                Spacer()
-                Button(action: {
-                    Task {
-                        if let selectedIndex = viewModel.selectedKabinettNumber {
-                            let selectedKabinettNumber = viewModel.availablekabinettNumbers[selectedIndex]
-                            
-                            let success = await viewModel.startLoginUser(
-                                with: viewModel.userName,
-                                kabinettNumber: selectedKabinettNumber
-                            )
-                            
-                            if success {
-                                viewModel.showSignUpFlow = false
-                            } else {
-                                viewModel.signUpError = "회원 가입에 실패했어요. 다시 시도해주세요."
-                                viewModel.showAlert = true
-                            }
+                    .disabled(viewModel.selectedKabinettNumber == nil)
+                    .padding(.horizontal, horizontalPadding)
+                    .alert(
+                        "오류",
+                        isPresented: $viewModel.showAlert
+                    ) {
+                        Button("확인", role: .cancel) {
                         }
+                    } message: {
+                        Text(viewModel.signUpError ?? "회원 가입 오류가 발생했어요. 카비넷 팀에게 알려주세요.")
                     }
-                }) {
-                    Text("시작하기")
-                        .fontWeight(.semibold)
-                        .font(.system(size: 16))
-                        .foregroundColor(.white)
-                        .frame(width: geometry.size.width * 0.86, height: 56)
-                        .background(RoundedRectangle(cornerRadius: 16)
-                            .fill(viewModel.selectedKabinettNumber != nil ? Color.primary900 : Color.primary300))
-                }
-                .disabled(viewModel.selectedKabinettNumber == nil)
-                .padding(.horizontal, geometry.size.width * 0.06)
-                .alert(
-                    "오류",
-                    isPresented: $viewModel.showAlert
-                ) {
-                    Button("확인", role: .cancel) {
-                    }
-                } message: {
-                    Text(viewModel.signUpError ?? "회원 가입 오류가 발생했어요. 카비넷 팀에게 알려주세요.")
-                }
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        Button(action: {
-                            dismiss()
-                        }) {
-                            HStack {
-                                Image(systemName: "chevron.left")
-                                    .font(.system(size: 18, weight: .semibold))
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarLeading) {
+                            Button(action: {
+                                dismiss()
+                            }) {
+                                HStack {
+                                    Image(systemName: "chevron.left")
+                                        .font(.system(size: 18, weight: .semibold))
+                                }
+                                .foregroundColor(.primary900)
                             }
-                            .foregroundColor(.primary900)
                         }
                     }
                 }
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color.background)
         }
         .navigationBarBackButtonHidden()
         .task {
