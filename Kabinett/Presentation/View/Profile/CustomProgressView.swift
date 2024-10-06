f204321bf8878a1ac3c6a4e960bfdd224306ef04
YuleGlycerine <figurefig13@gmail.com>
2024-10-06 16:48:50 +0900

    Feat: CustomProgressView 이미지 첨부

---
+++ Kabinett/Presentation/View/Profile/CustomProgressView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomProgressView.swift
+//  Kabinett
+//
+//  Created by T0MA on 10/3/24.
+//
+
+import SwiftUI
+
+struct CustomProgressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomProgressView()
+}
