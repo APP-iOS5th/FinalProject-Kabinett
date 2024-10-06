e0ccad3ba44bddf7c6826164c169029f3ecce9b9
jinwoong <baseball0020@gmail.com>
2024-10-06 15:46:58 +0900

    Feat: 의존성 등록 단위인 `Module` 추가

---
+++ Kabinett/Application/DIContainer/Module.swift
@@ -0,0 +1,19 @@
+//
+//  Module.swift
+//  Kabinett
+//
+//  Created by jinwoong Kim on 10/6/24.
+//
+
+import Foundation
+
+final class Module {
+    let name: String
+    let resolve: () -> Any
+    var resolved: Any? = nil
+    
+    init<T: InjectionKey>(_ name: T.Type, _ resolve: @escaping () -> Any) {
+        self.name = String(describing: name)
+        self.resolve = resolve
+    }
+}
