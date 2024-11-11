aeabf0d0261308269a1df9c02c5840c4ddfe0210
uunwon <chea2089@naver.com>
2024-11-11 16:53:29 +0900

    Feat: 동봉한 사진 저장 기능

--- Kabinett/Presentation/View/LetterBox/PhotoDetailView.swift
+++ Kabinett/Presentation/View/LetterBox/PhotoDetailView.swift
@@ -24,7 +24,7 @@ struct PhotoDetailView: View {
             VStack {
                 Spacer()
                 Button(action: {
-                    // 사진 저장
+                    savePhotoToAlbum()
                 }) {
                     Image(systemName: "square.and.arrow.down")
                         .font(.system(size: 20, weight: .medium))
@@ -50,5 +50,18 @@ struct PhotoDetailView: View {
             }
         }
         .navigationBarBackButtonHidden()
+    }
+    
+    func savePhotoToAlbum() {
+        guard let url = URL(string: photoUrl) else { return }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+                }
+            } else {
+                print("사진을 불러오지 못했습니다.", error?.localizedDescription ?? "알 수 없는 오류")
+            }
+        }.resume()
     }
 }
