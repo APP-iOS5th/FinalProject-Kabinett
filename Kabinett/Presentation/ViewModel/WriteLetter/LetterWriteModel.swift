d005f488373b1d1dd8ade18935e03f2100b3033f
song <nadana0929@gmail.com>
2025-01-20 22:32:59 +0900

    Refactor: `DataSource` 열거형 케이스 이름을 수정

--- Kabinett/Presentation/ViewModel/WriteLetter/LetterWriteModel.swift
+++ Kabinett/Presentation/ViewModel/WriteLetter/LetterWriteModel.swift
@@ -27,10 +27,10 @@ class LetterWriteModel: ObservableObject {
     @Published var date: Date = Date()
     @Published var stationeryImageUrlString: String? = nil
     @Published var isRead: Bool = false
-    @Published var dataSource: DataSource = .fromLetterWriting
+    @Published var dataSource: DataSource = .writeLetter
     
     enum DataSource {
-        case fromLetterWriting
-        case fromImagePicker
+        case writeLetter
+        case importLetter
     }
 }
