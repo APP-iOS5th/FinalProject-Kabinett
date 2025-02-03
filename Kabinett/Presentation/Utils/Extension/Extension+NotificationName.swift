41100132098698ae5bf34d9124d7b7c17cf9f952
the-hye <ganzi410@gmail.com>
2025-02-03 20:12:55 +0900

    Feat: Notification Extension 추가, ToastViewModel 추가

---
+++ Kabinett/Presentation/Utils/Extension/Extension+NotificationName.swift
@@ -0,0 +1,12 @@
+//
+//  Extension+NotificationName.swift
+//  Kabinett
+//
+//  Created by JIHYE SEOK on 2/3/25.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let showToast = Notification.Name("showToast")
+}
