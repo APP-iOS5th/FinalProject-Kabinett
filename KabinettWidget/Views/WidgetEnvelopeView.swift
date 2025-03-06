4c62dc1b9e118a1e72cad8555da9f3564c1d1e06
Jihye Seok <jeje@Jihyeui-MacBookPro.local>
2025-03-06 12:17:18 +0900

    Feat: WidgetEnvelopeView 파일 생성

---
+++ KabinettWidget/Views/WidgetEnvelopeView.swift
@@ -0,0 +1,18 @@
+//
+//  WidgetEnvelopeView.swift
+//  KabinettWidgetExtension
+//
+//  Created by Jihye Seok on 3/6/25.
+//
+
+import SwiftUI
+
+struct WidgetEnvelopeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WidgetEnvelopeView()
+}
