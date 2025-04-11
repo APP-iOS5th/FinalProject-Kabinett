ebf23946deba6d0c0db4f303ff0704caa054dfcc
the-hye <ganzi410@gmail.com>
2025-04-11 17:16:53 +0900

    Feat: WidgetView 수정

--- KabinettWidget/KabinettWidget.swift
+++ KabinettWidget/KabinettWidget.swift
@@ -8,53 +8,55 @@
 import WidgetKit
 import SwiftUI
 
-struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), emoji: "😀")
+struct WidgetLetterProvider: TimelineProvider {
+    func placeholder(in context: Context) -> WidgetLetterEntry {
+        WidgetLetterEntry(date: Date(), letters: [])
     }
     
     // 위젯 표기 시 미리보기로 보이는 화면
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), emoji: "😀")
+    func getSnapshot(in context: Context, completion: @escaping (WidgetLetterEntry) -> ()) {
+        let letters = WidgetLetterStorage.shared.loadWidgetLetters()
+        let entry = WidgetLetterEntry(date: Date(), letters: letters)
         completion(entry)
     }
     // 정의한 타임라인에 맞게 업데이트해서 보여질 내용
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, emoji: "😀")
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
+    func getTimeline(in context: Context, completion: @escaping (Timeline<WidgetLetterEntry>) -> ()) {
+        let letters = WidgetLetterStorage.shared.loadWidgetLetters()
+        let entry = WidgetLetterEntry(date: Date(), letters: letters)
+        let timeline = Timeline(entries: [entry], policy: .atEnd)
         completion(timeline)
     }
 }
 
-struct SimpleEntry: TimelineEntry {
+struct WidgetLetterEntry: TimelineEntry {
     let date: Date
-    let emoji: String
+    let letters: [WidgetLetter]
 }
 
 struct KabinettWidgetEntryView : View {
-    var entry: Provider.Entry
+    var entry: WidgetLetterProvider.Entry
 
     var body: some View {
         ZStack {
-            ForEach(Array(WidgetLetterStub.sampleLetters3.reversed().enumerated()), id: \.element.id) { index, letter in
-                let (xOffset, yOffset, rotation) = LayoutHelper.calculateWidgetOffsetAndRotation(for: index, totalCount: WidgetLetterStub.sampleLetters3.count)
+            ForEach(Array(entry.letters.reversed().enumerated()), id: \.element.id) { index, letter in
+                let (xOffset, yOffset, rotation) = LayoutHelper.calculateWidgetOffsetAndRotation(for: index, totalCount: entry.letters.count)
                 
-                WidgetEnvelopeView()
-                    .offset(x: xOffset, y: yOffset)
-                    .rotationEffect(.degrees(rotation))
-                    .zIndex(Double(-index))
+                if let letter = entry.letters.first {
+                    WidgetEnvelopeView(letter: letter)
+                        .offset(x: xOffset, y: yOffset)
+                        .rotationEffect(.degrees(rotation))
+                        .zIndex(Double(-index))
+                }
             }
             RedSticker()
         }
+//        VStack(alignment: .leading) {
+//                    ForEach(entry.letters.prefix(3), id: \.id) { letter in
+//                        Text("\(letter.fromUserName): \(letter.postScript)")
+//                            .font(.caption)
+//                    }
+//                }
+//                .padding()
     }
 }
 
@@ -62,11 +64,12 @@ struct KabinettWidget: Widget {
     let kind: String = "KabinettWidget"
 
     var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+        StaticConfiguration(kind: kind, provider: WidgetLetterProvider()) { entry in
             if #available(iOS 17.0, *) {
                 KabinettWidgetEntryView(entry: entry)
                     .containerBackground(Color.widgetBackground, for: .widget)
-            } else {
+            }
+            else {
                 KabinettWidgetEntryView(entry: entry)
                     .padding()
                     .background(Color.widgetBackground)
@@ -78,9 +81,9 @@ struct KabinettWidget: Widget {
     }
 }
 
-#Preview(as: .systemMedium) {
-    KabinettWidget()
-} timeline: {
-    SimpleEntry(date: .now, emoji: "😀")
-    SimpleEntry(date: .now, emoji: "🤩")
-}
+//#Preview(as: .systemMedium) {
+//    KabinettWidget()
+//} timeline: {
+//    WidgetLetterEntry(date: .now, emoji: "😀")
+//    WidgetLetterEntry(date: .now, emoji: "🤩")
+//}
